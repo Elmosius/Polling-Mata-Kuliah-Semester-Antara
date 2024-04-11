427695e8ac292e2b5f1a4a08e63f9c2940f0c00b
Josephine Alvina L <josephinealvinaluwia@gmail.com>
2024-04-11 21:55:03 +0700

    Ubah design Mata Kuliah

--- main/resources/views/polling/index.blade.php
+++ main/resources/views/polling/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div
-            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 dashboard rounded-1">
             <h3 class="h3">Polling Mata Kuliah Semester Antara</h3>
         </div>
         @if(session()->has('success'))
